cc13cf7829d3d7c694f639ef10ddc5c09c3c0d34
stefaxel <62843391+stefaxel@users.noreply.github.com>
2022-10-05 19:49:18 +0100

    Using input field
    Using the input field in unity to enter a name. This will be used to save a high score

--- Assets/Scripts/Main Menu/DataManager.cs
+++ Assets/Scripts/Main Menu/DataManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class DataManager : MonoBehaviour
 {
     public static DataManager Instance;
+    public string currentPlayer;
+    public int highScore;
+    public string bestPlayer;
 
     private void Awake()
     {
@@ -17,4 +21,36 @@ public class DataManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int savedScore;
+        public string savedPlayer;
+    }
+
+    public void SaveGameScore(string bestPlayer, int bestScore)
+    {
+        SaveData data = new SaveData();
+
+        data.savedPlayer = bestPlayer;
+        data.savedScore = highScore;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadSavedScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            bestPlayer = data.savedPlayer;
+            highScore = data.savedScore;
+        }
+    }
+
 }
--- Assets/Scripts/Main Menu/MenuUIHandler.cs
+++ Assets/Scripts/Main Menu/MenuUIHandler.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
-[DefaultExecutionOrder(1000)]
+//[DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
+    public string playerName;
+    public TMP_InputField inputField;
+    public TMP_Text highScoreText;
+    public Button startButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.interactable = false;
+        DataManager.Instance.LoadSavedScore();
+        highScoreText.text = $"Highscore - {DataManager.Instance.bestPlayer.ToUpper()}: {DataManager.Instance.highScore}";
     }
 
     public void StartNew()
@@ -29,4 +38,12 @@ Application.Quit();
 #endif
      
     }
+
+    public void StoreName()
+    {
+        startButton.interactable = true;
+        playerName = inputField.text;
+        DataManager.Instance.currentPlayer = playerName;
+    }
+
 }
